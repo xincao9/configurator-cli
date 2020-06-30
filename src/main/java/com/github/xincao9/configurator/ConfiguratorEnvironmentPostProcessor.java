d2b263563772cd2382f3f3bdcf645ca736039716
xincao9 <xincao9@gmail.com>
2020-06-30 21:16:39 +0800

    format

--- src/main/java/com/github/xincao9/configurator/ConfiguratorEnvironmentPostProcessor.java
+++ src/main/java/com/github/xincao9/configurator/ConfiguratorEnvironmentPostProcessor.java
@@ -17,7 +17,6 @@ package com.github.xincao9.configurator;
 
 import com.github.xincao9.configurator.dkv.DkvException;
 import org.apache.commons.lang3.StringUtils;
-import org.jetbrains.annotations.NotNull;
 import org.slf4j.Logger;
 import org.slf4j.LoggerFactory;
 import org.springframework.boot.SpringApplication;
